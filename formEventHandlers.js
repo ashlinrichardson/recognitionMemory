1f5b4bb615f7d39c499fd560da5163b3900cb98d
ashlinrichardson <ashlin.richardson@gmail.com>
2016-10-21 16:34:02 -0700

    Continuing..
    Continuing..

---
+++ formEventHandlers.js
@@ -0,0 +1,11 @@
+	function togglePool1(){
+		alert('hi');
+			var e = document.getElementById("pool1"); e.value = !e.value;	
+		}
+		function togglePool2(){
+			var e = document.getElementById("pool2"); e.value = !e.value;	
+		}
+		function togglePool3(){
+			var e = document.getElementById("pool3"); e.value = !e.value;	
+		}
+
